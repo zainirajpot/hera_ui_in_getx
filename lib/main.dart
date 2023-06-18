9f7b554742f3f9e4813780c237719ac5f89e6508
Zainab Rajpot <zainichohan712@gmail.com>
2023-06-18 19:20:23 +0500

    i have updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Hera UI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
